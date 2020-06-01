09eda20873dfcd9bfb4290804da9f9b1c233cdb9
KelvinMitaki <mitakikelvin1@gmail.com>
2020-06-01 15:14:42 +0300

    added the addtocart method on the user class model

--- models/user.js
+++ models/user.js
@@ -3,12 +3,25 @@ const mongodb = require("mongodb");
 const { getDb } = require("../utils/database");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   async save() {
     await getDb().db().collection("users").insertOne(this);
+  }
+  async addToCart(product) {
+    // const cartProductIndex=this.cart.items.findIndex(pro=>pro._id===product._id)
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    await getDb()
+      .db()
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
   static async findById(id) {
     return await getDb()
