324cd9998a4c01c0c5e65dc9ada8fc80d502b09e
KelvinMitaki <mitakikelvin1@gmail.com>
2020-06-02 14:16:57 +0300

    added a product schema

--- models/product.js
+++ models/product.js
@@ -1,56 +1,11 @@
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
-const { getDb } = require("../utils/database");
-const ObjectId = mongodb.ObjectId;
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true }
+});
 
-module.exports = class Product {
-  constructor(title, imageUrl, description, price, userId) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-    this.userId = userId;
-  }
-
-  async save() {
-    const result = await getDb()
-      .db("shop")
-      .collection("products")
-      .insertOne(this);
-    return result;
-  }
-  static async findByIdAndDelete(id) {
-    await getDb()
-      .db()
-      .collection("products")
-      .deleteOne({ _id: new ObjectId(id) });
-    // TO BE CONFIRMED
-    await getDb()
-      .db()
-      .collection("users")
-      .updateOne(
-        {},
-        { $unset: { "cart.items.$[el]": "" } },
-        { arrayFilters: [{ el: new ObjectId(id) }] }
-      );
-  }
-  static async fetchAll(cb) {
-    const result = await getDb().db().collection("products").find({}).toArray();
-    cb(result);
-  }
-
-  static async findById(id, cb) {
-    const result = await getDb()
-      .db()
-      .collection("products")
-      .findOne({ _id: new ObjectId(id) });
-
-    cb(result);
-  }
-  static async findByIdAndUpdate(id, update) {
-    await getDb()
-      .db()
-      .collection("products")
-      .updateOne({ _id: new ObjectId(id) }, { $set: update });
-  }
-};
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
