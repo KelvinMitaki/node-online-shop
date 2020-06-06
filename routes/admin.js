d6d67df324b286b07d1e118b9ff2f44b5212d9f6
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-06 22:51:08 +0300

    added authentication where users not logged in cannot be able to perform certain tasks

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,20 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin");
+const auth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", adminController.getAddProduct);
-router.get("/edit-product/:productId", adminController.getEditProduct);
+router.get("/add-product", auth, adminController.getAddProduct);
+router.get("/edit-product/:productId", auth, adminController.getEditProduct);
 
 // /admin/products => GET
-router.get("/products", adminController.getProducts);
+router.get("/products", auth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", adminController.postAddProduct);
-router.post("/edit-product", adminController.postEditProduct);
-router.post("/delete-product", adminController.postDeleteProduct);
+router.post("/add-product", auth, adminController.postAddProduct);
+router.post("/edit-product", auth, adminController.postEditProduct);
+router.post("/delete-product", auth, adminController.postDeleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const shopController = require("../controllers/shop");
+const auth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -9,13 +10,13 @@ router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 
-router.get("/cart", shopController.getCart);
-router.post("/cart", shopController.postCart);
+router.get("/cart", auth, shopController.getCart);
+router.post("/cart", auth, shopController.postCart);
 
-router.get("/orders", shopController.getOrders);
-router.get("/checkout", shopController.getCheckout);
+router.get("/orders", auth, shopController.getOrders);
+router.get("/checkout", auth, shopController.getCheckout);
 
-router.post("/cart-delete-item", shopController.deleteCart);
-router.post("/create-order", shopController.postOrder);
+router.post("/cart-delete-item", auth, shopController.deleteCart);
+router.post("/create-order", auth, shopController.postOrder);
 
 module.exports = router;
